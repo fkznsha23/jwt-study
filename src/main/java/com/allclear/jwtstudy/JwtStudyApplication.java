f4d60ea2c5811a97f62f6a8fd2609daeb8f6de83
Eunjung <fkznsha23@gmail.com>
2024-09-04 18:55:41 +0900

    [Chore] 환경설정 셋팅

---
+++ src/main/java/com/allclear/jwtstudy/JwtStudyApplication.java
@@ -0,0 +1,14 @@
+package com.allclear.jwtstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtStudyApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(JwtStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allclear/jwtstudy/JwtStudyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.allclear.jwtstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
